9c46e479b8e3f39a50ac35cdefd9c0639259726b
lepatrick714 <lle018@ucr.edu>
2017-01-22 02:58:57 -0800

    Yes

--- sliders.js
+++ sliders.js
@@ -1,4 +1,12 @@
 $(function() { 
-    $("#slider-1").slider(); 
+    $("#slider-1").slider({
+        range:true, 
+        min:2017, 
+        max:2060, 
+        slide:function(event, ui) { 
+            $("#slidevalue")
+                .val(ui.values[1]); 
+        }
+    });
 }); 
 
